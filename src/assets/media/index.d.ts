92f832b2d464438e651e22203ffc7efc112cbf46
Munish Kumar <send2munish@gmail.com>
2024-04-20 20:36:29 +0530

    Updated types

--- src/assets/media/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.png" {
-	const value: string
-	export = value
-}
-declare module "*.jpg" {
-	const value: string
-	export = value
-}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,9 +2,7 @@
 
 interface ImportMetaEnv {
 	readonly DB_URI: string
-	readonly VITE_ACCESS_TOKEN: string
-	readonly VITE_LOG_API: string
-	readonly VITE_DASHBOARD_KEY: string
+	readonly VITE_API_URL: string
 }
 
 interface ImportMeta {
